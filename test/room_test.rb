2be3ce027f8c4d210c7e3f41f7f78709db73eedf
DeMarcus Kirby <tkd024@gmail.com>
2019-10-09 09:49:54 -0600

    Create Room based on written tests

--- test/room_test.rb
+++ test/room_test.rb
@@ -3,20 +3,20 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/room.rb'
 
-class RoomTest < Minitest::RoomTest
+class RoomTest < Minitest::Test
   def setup
-    room = Room.new(:bedroom, 10, 15)
+    @room = Room.new(:bedroom, 10, 15)
   end
 
   def test_it_exist
-    assert_instance_of Room, room
+    assert_instance_of Room, @room
   end
 
   def test_it_has_a_category
-    assert_equal :bedroom, room.category
+    assert_equal :bedroom, @room.category
   end
 
   def test_area_method
-    assert_equal 150, room.area
+    assert_equal 150, @room.area
   end
 end
