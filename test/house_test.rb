bf8727aa9ada7ace351d26f3504d63de0625994b
DeMarcus Kirby <tkd024@gmail.com>
2019-10-09 10:41:44 -0600

    Update house test for iteration 4

--- test/house_test.rb
+++ test/house_test.rb
@@ -59,4 +59,38 @@ class HouseTest < Minitest::Test
     assert_equal 1865, @house.area
   end
 
+  def test_price_per_square_foot_method
+
+    @house.add_room(@room)
+    @house.add_room(@room2)
+    assert_equal 1379.31, @house.price_per_square_foot
+
+    @house.add_room(@room3)
+    assert_equal 601.50, @house.price_per_square_foot
+
+    @house.add_room(@room4)
+    assert_equal 214.47, @house.price_per_square_foot
+  end
+
+  def test_sort_by_area_method
+    @house.add_room(@room)
+    @house.add_room(@room2)
+    @house.add_room(@room3)
+    @house.add_room(@room4)
+    assert_equal [@room, @room2, @room3, @room4], @house.rooms
+    assert_equal [@room4, @room3, @room2, @room1], @house.sort_by_area
+  end
+
+  def test_rooms_by_category_method
+    @house.add_room(@room)
+    @house.add_room(@room2)
+    @house.add_room(@room3)
+    @house.add_room(@room4)
+    assert_equal {
+    :bedroom => [@room1, @room2],
+    :living_room => [@room3]  
+    :basement => [@room4]
+    },
+    house.rooms_by_category
+  end
 end
