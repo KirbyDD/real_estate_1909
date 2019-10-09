ff0d7d050ee6acc9425084c9658f560e4c2d6d6e
DeMarcus Kirby <tkd024@gmail.com>
2019-10-09 09:45:31 -0600

    Add Iteration One Set of tests

--- test/room_test.rb
+++ test/room_test.rb
@@ -5,6 +5,18 @@ require_relative '../lib/room.rb'
 
 class RoomTest < Minitest::RoomTest
   def setup
-    
+    room = Room.new(:bedroom, 10, 15)
+  end
+
+  def test_it_exist
+    assert_instance_of Room, room
+  end
+
+  def test_it_has_a_category
+    assert_equal :bedroom, room.category
+  end
+
+  def test_area_method
+    assert_equal 150, room.area
   end
 end
